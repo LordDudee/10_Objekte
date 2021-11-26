5b35edf43beaac7441d64167c6622627065a4b08
Dude <michael_runschke@gmx.de>
2021-11-26 12:30:08 +0100

    1.Objekt + Properties

--- script_01.js
+++ script_01.js
@@ -1,18 +1,43 @@
-
 /***** Erinnerung: Arrays *******/
-let arr;
+
+// let arr;
 // arr = ["Ich","bin","Max"];
 // output(arr);
 // output(arr[2]);
 
-arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]]; // 2 Arrays erstes mit Max, zweites mit Moritz
-output(arr[0][2]); // aus dem ersten Array das letzte Wort: Max
-output(arr[1][2]); // aus dem zweiten Arrsy das letzte Wort: Moritz
-
+// arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]]; // 2 Arrays erstes mit Max, zweites mit Moritz
+// output(arr[0][2]); // aus dem ersten Array das letzte Wort: Max
+// output(arr[1][2]); // aus dem zweiten Arrsy das letzte Wort: Moritz
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+const person = 
+{
+        firstName: "Patrick",  // strings
+        familyName: "Polgar",  // strings
+        salary:[120000,160000],  // Arrays
+        permission: true,
+        sayHello:                       // Objekt kann ein verhalten haben
+                function () {
+                        return "Hi, ich bin " + this.firstName;
+                }
 
+};
+
+output(person);
+output(person.firstName);
+person.firstName = "Patrik";
+output(person.familyName);
+output(person.salary[1]);
+output(person.permission);
+output(person.sayHello());
+// output(person.firstName);
+
+const txt =     "Ich bin " + person.firstName + " " +
+                person.familyName + " und verdiene " +
+                person.salary[1] + " p.a."
+
+output(txt);
 
 
 /** Ausgabe */
